97874e29fdcea83f7c3d81682a06027e3d8f277f
Windows 95 SE <Windows 95 SE@DESKTOP-PNDE539>
2018-10-15 17:41:30 +0200

    Add project files.

---
+++ AlgZad/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgZad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
